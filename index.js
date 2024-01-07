08edf78d35fcae11be91238bab7aacc84fb4ce53
clemens <dcleme51@gmail.com>
2024-01-07 23:28:30 +0000

    adding all the time blocks and if else statement for colour block

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ document.getElementById("currentDay").textContent = currentTime;
 
 // Present timeblocks for standard business hours when the user scrolls down.
 
-
-
 // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
 function updateColour(){
     let currentTime = dayjs().hour();
@@ -14,9 +12,20 @@ function updateColour(){
     $(".time-block").each(function(){
         let apptTime = parseInt($(this).attr("id"));
         console.log("apptTime=", apptTime);  
-    })
+   
 
+if(apptTime < currentTime){
+    $(this).addClass("past");
+} else if(apptTime === currentTime){
+    $(this).addClass("present");
+    $(this).removeClass("past");
 
+} else{
+    $(this).addClass("future");
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+}
+});
 }
 
 updateColour();
@@ -35,3 +44,9 @@ localStorage.setItem(hour, text);
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
